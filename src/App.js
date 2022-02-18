92b5f956a8e5e965e95ea7e6d26a84de3104bc2b
joscord <josecorderodev@gmail.com>
2022-02-18 13:28:33 -0300

    feat: modified App.js to include all project route components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import {
+	BrowserRouter,
+	Switch,
+	Route,
+} from 'react-router-dom/cjs/react-router-dom.min';
+import { Dashboard } from './pages/dashboard/Dashboard';
+import { Create } from './pages/create/Create';
+import { Login } from './pages/login/Login';
+import { Signup } from './pages/signup/Signup';
+import { Project } from './pages/project/Project';
+
 import './App.css';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<BrowserRouter>
+				<div className='container'>
+					<Switch>
+						<Route exact path='/'>
+							<Dashboard />
+						</Route>
+						<Route path='/create'>
+							<Create />
+						</Route>
+						<Route path='/projects/:id'>
+							<Project />
+						</Route>
+						<Route path='/login'>
+							<Login />
+						</Route>
+						<Route path='/signup'>
+							<Signup />
+						</Route>
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;
