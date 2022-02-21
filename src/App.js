cc92dd3fcb00fecc76da4d20a6e4cd3da9631896
joscord <josecorderodev@gmail.com>
2022-02-21 20:09:26 -0300

    feat: guarded routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// Importamos el componente Redirect
 import {
 	BrowserRouter,
 	Switch,
 	Route,
+	Redirect,
 } from 'react-router-dom/cjs/react-router-dom.min';
 
 import Dashboard from './pages/dashboard/Dashboard';
@@ -12,33 +14,39 @@ import Signup from './pages/signup/Signup';
 import Navbar from './components/navbar/Navbar';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
+import { useAuthContext } from './hooks/useAuthContext';
 
 const App = () => {
+	// Destructuramos la propiedad user de nuestro contexto. También tomamos authIsReady, recordemos que esta propiedad es una propiedad que nos sirve para no cargar la app hasta que sepamos si estamos logueados o no en Firebase. Dependiendo de esto vemos qué contenido mostramos
+	const { user, authIsReady } = useAuthContext();
+
 	return (
 		<div className='App'>
-			<BrowserRouter>
-			<Sidebar/>
-				<div className='container'>
-					<Navbar />
-					<Switch>
-						<Route exact path='/'>
-							<Dashboard />
-						</Route>
-						<Route path='/create'>
-							<Create />
-						</Route>
-						<Route path='/projects/:id'>
-							<Project />
-						</Route>
-						<Route path='/login'>
-							<Login />
-						</Route>
-						<Route path='/signup'>
-							<Signup />
-						</Route>
-					</Switch>
-				</div>
-			</BrowserRouter>
+			{authIsReady && (
+				<BrowserRouter>
+					<Sidebar />
+					<div className='container'>
+						<Navbar />
+						<Switch>
+							<Route exact path='/'>
+								{user ? <Dashboard /> : <Redirect to={'/login'}/>}
+							</Route>
+							<Route path='/create'>
+								{user ? <Create /> : <Redirect to={'/login'}/>}
+							</Route>
+							<Route path='/projects/:id'>
+								{user ? <Project /> : <Redirect to={'/login'}/>}
+							</Route>
+							<Route path='/login'>
+								{!user ? <Login /> : <Redirect to={'/dashboard'}/>}
+							</Route>
+							<Route path='/signup'>
+								{!user ? <Signup /> : <Redirect to={'/dashboard'}/>}
+							</Route>
+						</Switch>
+					</div>
+				</BrowserRouter>
+			)}
 		</div>
 	);
 };
