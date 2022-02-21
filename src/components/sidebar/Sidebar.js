8e7de2703f58e8a6f57e54972b06f07c00da34c8
joscord <josecorderodev@gmail.com>
2022-02-21 20:43:10 -0300

    feat: added Avatar.js into Sidebar.js

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,25 +2,32 @@ import './Sidebar.css';
 import dashboardIcon from '../../assets/dashboard_icon.svg';
 import addIcon from '../../assets/add_icon.svg';
 import { NavLink } from 'react-router-dom';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import Avatar from '../avatar/Avatar';
 
 const Sidebar = () => {
+	const { user } = useAuthContext();
+
 	return (
 		<div className='sidebar'>
 			<div className='sidebar-content'>
 				<div className='user'>
-					<p>user here</p>
+					{user && (
+						<>
+							<Avatar src={user.photoURL}/>
+							<p>Hello {user.displayName}</p>
+						</>
+					)}
 				</div>
 				<nav className='links'>
 					<ul>
 						<li>
-							{/* AÑadimos NavLink porque le da una clase activa a los links */}
 							<NavLink exact to={'/'}>
 								<img src={dashboardIcon} alt='dashboard-icon' />
 								<span>Dashboard</span>
 							</NavLink>
 						</li>
 						<li>
-							{/* AÑadimos NavLink porque le da una clase activa a los links */}
 							<NavLink to={'/create'}>
 								<img src={addIcon} alt='add-project-icon' />
 								<span>New Project</span>
