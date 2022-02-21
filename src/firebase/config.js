64de2a38e8b301e38246c07dc0dc6a03f96fc57f
joscord <josecorderodev@gmail.com>
2022-02-21 13:36:35 -0300

    update: modified config.js to initialize Firebase Storage service. Modified Signup.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,8 @@
-// importamos firebase y los servicios que ocuparemos para el proyecto
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage'
 
-// Nuestro objeto de configuración facilitado por firebase
 const firebaseConfig = {
 	apiKey: 'AIzaSyCqDBM7f08BoaWoK_J7_TncjmjzBKVMPqs',
 	authDomain: 'projectm-backend.firebaseapp.com',
@@ -13,15 +12,14 @@ const firebaseConfig = {
 	appId: '1:179616182652:web:967889f9fce65bc99e34f9',
 };
 
-// Inicializamos firebase
 firebase.initializeApp(firebaseConfig);
-// Inicializamos firestore
+
 const projectFirestore = firebase.firestore();
-// inicializamos la autenticación
+
 const projectAuth = firebase.auth();
 
-// Vamos a preparar nuestra función de timestamps
+const projectStorage = firebase.storage();
+
 const timestamp = firebase.firestore.Timestamp;
 
-// Exportamos los servicios
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -3,15 +3,45 @@ import './Signup.css';
 import React, { useState } from 'react';
 
 const Signup = () => {
-	// Creamos piezas de estado para manejar name, email, password, confirmPassword y la imagen que subirá el usuario
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
 	const [thumbnail, setThumbnail] = useState(null);
-	const handleSubmit = {
-	}
-	
+	// Manejaremos un estado para el error de la selección del archivo
+	const [thumbnailError, setThumbnailError] = useState(null);
+	const handleSubmit = {};
+	// Definimos ls función para cuando se selecciona un archivo
+	const handleFileChange = e => {
+		// Primero vamos a resetear el estado por si había otra imagen seleccionada de antes
+		setThumbnail(null);
+		// Podemos ver qué archivo seleccionó el usuario. files retorna un arreglo de archivos (esto porque uno puede seleecionar varios archivos). Pero sólo escogeremos el primero del arreglo a través de su índice
+		let selectedFile = e.target.files[0];
+		// Podemos usar la propiedad type del archivo para verificar que subió una imagen y no otro tipo de archivo.
+		// Primero verificamos por undefined. El valor es undefined si el usuario en lugar de seleccionar un archivo da a la opción cancel
+		if (!selectedFile) {
+			setThumbnailError('Please select a file');
+			return;
+		}
+		// Luego el segundo check es ver que sea la imagen. Usando el método includes vemos que la propiedad type incluya el string image (independiente si luego sigue png o jpeg u otro formato). Y si no lo incluye actualizamos el error
+		if (!selectedFile.type.includes('images')) {
+			setThumbnailError('Selected file must be an image');
+			return;
+		}
+		// Lo siguiente es checkear el tamaño en base a la propiedad size. No puedes ser mayor de 100000 bytes	
+		if (selectedFile.size > 100000) {
+			setThumbnailError('Image file size must be less than 100kb');
+			return;
+		}
+		// Si pasamos todo los checks podemos seguir con la función. Primero reseteamos el error
+		setThumbnailError(null);
+		// Actualizamos el estado thumbnail
+		setThumbnail(selectedFile);
+
+
+
+	};
+
 	return (
 		<form className='auth-form' onSubmit={handleSubmit}>
 			<h2>Sign up</h2>
@@ -53,11 +83,9 @@ const Signup = () => {
 			</label>
 			<label>
 				<span>Profile Picture: </span>
-				<input
-					type='file'
-					required
-				/>
-			</label>
+				<input type='file' required onChange={handleFileChange} />
+				{thumbnailError && <div className='error'>{thumbnailError}</div>}
+			</label>	
 			<button>Sign Up</button>
 		</form>
 	);
