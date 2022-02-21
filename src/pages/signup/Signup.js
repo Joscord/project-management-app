9f1d694f3f6c295468c7a70e212ef566a553077d
joscord <josecorderodev@gmail.com>
2022-02-21 16:49:58 -0300

    update: modified Signup adding useSignup hook and a passwordError state

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,45 +1,48 @@
 import './Signup.css';
 
 import React, { useState } from 'react';
+import { useSignup } from '../../hooks/useSignup';
 
 const Signup = () => {
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
+	const [passwordError, setPasswordError] = useState(null);
 	const [thumbnail, setThumbnail] = useState(null);
-	// Manejaremos un estado para el error de la selección del archivo
 	const [thumbnailError, setThumbnailError] = useState(null);
-	const handleSubmit = {};
-	// Definimos ls función para cuando se selecciona un archivo
+	// Destructuramos de useSignup
+	const { signup, error, isPending } = useSignup();
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		setPasswordError(null);
+		if (password !== confirmPassword) {
+			setPasswordError('Passwords do not match');
+			return;
+		}
+
+		// Invocamos al método signup
+		signup(email, password, name, thumbnail);
+	};
+
 	const handleFileChange = e => {
-		// Primero vamos a resetear el estado por si había otra imagen seleccionada de antes
 		setThumbnail(null);
-		// Podemos ver qué archivo seleccionó el usuario. files retorna un arreglo de archivos (esto porque uno puede seleecionar varios archivos). Pero sólo escogeremos el primero del arreglo a través de su índice
 		let selectedFile = e.target.files[0];
-		// Podemos usar la propiedad type del archivo para verificar que subió una imagen y no otro tipo de archivo.
-		// Primero verificamos por undefined. El valor es undefined si el usuario en lugar de seleccionar un archivo da a la opción cancel
 		if (!selectedFile) {
 			setThumbnailError('Please select a file');
 			return;
 		}
-		// Luego el segundo check es ver que sea la imagen. Usando el método includes vemos que la propiedad type incluya el string image (independiente si luego sigue png o jpeg u otro formato). Y si no lo incluye actualizamos el error
 		if (!selectedFile.type.includes('images')) {
 			setThumbnailError('Selected file must be an image');
 			return;
 		}
-		// Lo siguiente es checkear el tamaño en base a la propiedad size. No puedes ser mayor de 100000 bytes	
 		if (selectedFile.size > 100000) {
 			setThumbnailError('Image file size must be less than 100kb');
 			return;
 		}
-		// Si pasamos todo los checks podemos seguir con la función. Primero reseteamos el error
 		setThumbnailError(null);
-		// Actualizamos el estado thumbnail
 		setThumbnail(selectedFile);
-
-
-
 	};
 
 	return (
@@ -71,6 +74,7 @@ const Signup = () => {
 					onChange={e => setPassword(e.target.value)}
 					required
 				/>
+				{passwordError && <p className='error'>{passwordError}</p>}
 			</label>
 			<label>
 				<span>Confirm password: </span>
@@ -80,13 +84,19 @@ const Signup = () => {
 					onChange={e => setConfirmPassword(e.target.value)}
 					required
 				/>
+				{passwordError && <p className='error'>{passwordError}</p>}
 			</label>
 			<label>
 				<span>Profile Picture: </span>
 				<input type='file' required onChange={handleFileChange} />
 				{thumbnailError && <div className='error'>{thumbnailError}</div>}
-			</label>	
-			<button>Sign Up</button>
+			</label>
+			{isPending ? (
+				<button disabled>Loading...</button>
+			) : (
+				<button className='btn'>Sign Up</button>
+			)}
+			{error && <p className='error'>{error}</p>}
 		</form>
 	);
 };
