117cff04030f40b8046b93d8c1d58493330eb6a8
joscord <josecorderodev@gmail.com>
2022-02-22 01:02:07 -0300

    feat: modified OnlineUsers.js and OnlineUsers.css so it is styled different if their is an online user

--- src/components/onlineusers/OnlineUsers.js
+++ src/components/onlineusers/OnlineUsers.js
@@ -12,7 +12,8 @@ const OnlineUsers = () => {
 			<h2>All Users</h2>
             {error && <div className='error'>{error}</div>}
             {users && users.map(user => (
-                <div key={user.id} className='user-list-item'>
+                <div key={user.id} className='user-list-item'> 
+                    {user.online && <span className='online-user'/>}
                     <span>{user.displayName}</span>
                     <Avatar src={user.photoURL}/>
                 </div>
