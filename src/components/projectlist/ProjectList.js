19389fb82794682d9284399610425d21756b4d81
joscord <josecorderodev@gmail.com>
2022-02-22 18:09:36 -0300

    update: modified ProjectList.js and ProjectList.css

--- src/components/projectlist/ProjectList.js
+++ src/components/projectlist/ProjectList.js
@@ -1,11 +1,32 @@
 import React from 'react';
+// Importamos Avatar
+import Avatar from '../avatar/Avatar'
+// Importamos Link de react-router-dom
+import { Link } from 'react-router-dom';
 import './ProjectList.css';
 
+// Hay que transformar el timestamp de Firebase a una fecha y esa fecha a un string
 export const ProjectList = ({projects}) => {
 	return (
-		<div>
+		<div className='project-list'>
             {projects.length === 0 && <p>No projects yet</p>}
-			{projects.map(project => <p key={project.id}>{project.name}</p>)}
+			{projects.map(project => <Link key={project.id} to={`/projects/${project.id}`}>
+                <h4>
+                    {project.name}
+                </h4>
+                <p>
+                    Due by: {project.dueDate.toDate().toDateString()}
+                </p>
+                <div className='assigned-to'>Assigned to: 
+                    <ul>
+                        {project.assignedUsersList.map(user => (
+                            <li key={user.photoURL}>
+                                <Avatar src={user.photoURL}/>
+                            </li>
+                        ))}
+                    </ul>
+                 </div>
+            </Link>)}
 		</div>
 	);
 };
