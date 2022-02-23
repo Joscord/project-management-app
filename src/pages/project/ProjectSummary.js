cab0f549a883d971797618c88d58f9c2425018d9
joscord <josecorderodev@gmail.com>
2022-02-23 16:14:08 -0300

    feat: added a button to complete/delete a project

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,26 +1,37 @@
-import React from 'react'
-// Importamos el componente Avatar
-import Avatar from '../../components/avatar/Avatar';    
+import React from 'react';
+import Avatar from '../../components/avatar/Avatar';
+// importamos useFirestore para poder usar la función de borrar documentos
+import { useFirestore } from '../../hooks/useFirestore';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
-// Esperamos recibir el proyecto como prop
-export const ProjectSummary = ({project}) => {
-  return (
-    <div className='project-summary'>
-        <h2 className='page-title'>{project.name}</h2>
-        <p className='due-date'>
-            Project due by {project.dueDate.toDate().toDateString()}
-        </p>
-        <p className='details'>
-            {project.details}
-        </p>
-        <h4>Project is assigned to:</h4>
-        <div className='assigned-users'>
-            {project.assignedUsersList.map(user => (
-                <div key={user.id}>
-                    <Avatar src={user.photoURL}/>
-                </div>
-            ))}
-        </div>
-    </div>
-  )
-}
+export const ProjectSummary = ({ project }) => {
+	const { deleteDocument } = useFirestore('projects');
+    const { user } = useAuthContext(); 
+    const handleClick = e => {
+        deleteDocument(project.id); 
+    }
+    // Para ocultar el botón recordemos que el proyecto tiene una propiedad createdBy, dentro de esta está el id y podemos compararla con el id de la persona logueada
+	return (
+		<>  
+			<div className='project-summary'>
+				<h2 className='page-title'>{project.name}</h2>
+                <p>Author: {project.createdBy.displayName}</p>
+				<p className='due-date'>
+					Project due by {project.dueDate.toDate().toDateString()}
+				</p>
+				<p className='details'>{project.details}</p>
+				<h4>Project is assigned to:</h4>
+				<div className='assigned-users'>
+					{project.assignedUsersList.map(user => (
+						<div key={user.id}>
+							<Avatar src={user.photoURL} />
+						</div>
+					))}
+				</div>
+            {user.uid === project.createdBy.id && (
+                <button className='btn' onClick={handleClick}>Mark as Completed</button>
+            )}
+			</div>
+		</>
+	);
+};
