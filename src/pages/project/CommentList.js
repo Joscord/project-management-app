7d6096200b67263554e00e2e66add272b1954221
joscord <josecorderodev@gmail.com>
2022-02-24 12:55:17 -0300

    feat: added a date to the comments in CommentLIst

--- src/pages/project/CommentList.js
+++ src/pages/project/CommentList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 // Importamos el Avatar
 import Avatar from '../../components/avatar/Avatar';
+// Importamos la función formatDistanceToNow. Esta función revisa la fecha que le pasemos y calcula la distancia hacia la fecha actual
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'; 
 
 export const CommentList = ({ project }) => {
 	// Destructuramos los comentarios
@@ -16,7 +18,8 @@ export const CommentList = ({ project }) => {
                         <p>{comment.displayName}</p>
                     </div>
                     <div className='comment-date'>
-                        <p>Date Here</p>
+                        {/* El primer parámetro es una fecha. Como createdAt es un timestamp debemos transformarlo a una fecha con toDate(). El segundo argumento es un objeto con opciones. Pasamos la opción addSuffix: true para que añada la palabra ago al final */}
+                        <p>{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix: true})}</p>
                     </div>
                     <div className='comment-content'>
                         <p>{comment.content}</p>
